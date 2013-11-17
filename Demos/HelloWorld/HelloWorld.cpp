7c3e36f4f049302c768e76a6753dfd2d511ab660
Usagi Ito <usagi@WonderRabbitProject.net>
2013-11-17 17:01:35 +0900

    インデントルールをタブから2-SPACEに変更

--- Demos/HelloWorld/HelloWorld.cpp
+++ Demos/HelloWorld/HelloWorld.cpp
@@ -22,24 +22,24 @@ subject to the following restrictions:
 
 int main()
 {
-	///-----includes_end-----
+  ///-----includes_end-----
 
-	int i;
-	///-----initialization_start-----
+  int i;
+  ///-----initialization_start-----
 
-	///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
-        std::unique_ptr<btDefaultCollisionConfiguration> collisionConfiguration(new btDefaultCollisionConfiguration());
+  ///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
+  std::unique_ptr<btDefaultCollisionConfiguration> collisionConfiguration(new btDefaultCollisionConfiguration());
 
-	///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
-	std::unique_ptr<btCollisionDispatcher> dispatcher(new btCollisionDispatcher(collisionConfiguration.get()));
+  ///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+  std::unique_ptr<btCollisionDispatcher> dispatcher(new btCollisionDispatcher(collisionConfiguration.get()));
 
-	///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
-	std::unique_ptr<btBroadphaseInterface> overlappingPairCache(new btDbvtBroadphase());
+  ///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
+  std::unique_ptr<btBroadphaseInterface> overlappingPairCache(new btDbvtBroadphase());
 
-	///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
-	std::unique_ptr<btSequentialImpulseConstraintSolver> solver(new btSequentialImpulseConstraintSolver);
+  ///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
+  std::unique_ptr<btSequentialImpulseConstraintSolver> solver(new btSequentialImpulseConstraintSolver);
 
-	std::unique_ptr<btDiscreteDynamicsWorld> dynamicsWorld
+  std::unique_ptr<btDiscreteDynamicsWorld> dynamicsWorld
         ( new btDiscreteDynamicsWorld
           ( dispatcher.get()
           , overlappingPairCache.get()
@@ -48,132 +48,132 @@ int main()
           )
         );
 
-	dynamicsWorld->setGravity(btVector3(0,-10,0));
+  dynamicsWorld->setGravity(btVector3(0,-10,0));
 
-	///-----initialization_end-----
+  ///-----initialization_end-----
 
-	///create a few basic rigid bodies
-	btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(50.),btScalar(50.),btScalar(50.)));
+  ///create a few basic rigid bodies
+  btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(50.),btScalar(50.),btScalar(50.)));
 
-	//keep track of the shapes, we release memory at exit.
-	//make sure to re-use collision shapes among rigid bodies whenever possible!
-	btAlignedObjectArray<btCollisionShape*> collisionShapes;
+  //keep track of the shapes, we release memory at exit.
+  //make sure to re-use collision shapes among rigid bodies whenever possible!
+  btAlignedObjectArray<btCollisionShape*> collisionShapes;
 
-	collisionShapes.push_back(groundShape);
+  collisionShapes.push_back(groundShape);
 
-	btTransform groundTransform;
-	groundTransform.setIdentity();
-	groundTransform.setOrigin(btVector3(0,-56,0));
+  btTransform groundTransform;
+  groundTransform.setIdentity();
+  groundTransform.setOrigin(btVector3(0,-56,0));
 
-	{
-		btScalar mass(0.);
+  {
+    btScalar mass(0.);
 
-		//rigidbody is dynamic if and only if mass is non zero, otherwise static
-		bool isDynamic = (mass != 0.f);
+    //rigidbody is dynamic if and only if mass is non zero, otherwise static
+    bool isDynamic = (mass != 0.f);
 
-		btVector3 localInertia(0,0,0);
-		if (isDynamic)
-			groundShape->calculateLocalInertia(mass,localInertia);
+    btVector3 localInertia(0,0,0);
+    if (isDynamic)
+      groundShape->calculateLocalInertia(mass,localInertia);
 
-		//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
-		btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
-		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass,myMotionState,groundShape,localInertia);
-		btRigidBody* body = new btRigidBody(rbInfo);
+    //using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+    btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
+    btRigidBody::btRigidBodyConstructionInfo rbInfo(mass,myMotionState,groundShape,localInertia);
+    btRigidBody* body = new btRigidBody(rbInfo);
 
-		//add the body to the dynamics world
-		dynamicsWorld->addRigidBody(body);
-	}
+    //add the body to the dynamics world
+    dynamicsWorld->addRigidBody(body);
+  }
 
 
-	{
-		//create a dynamic rigidbody
+  {
+    //create a dynamic rigidbody
 
-		//btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
-		btCollisionShape* colShape = new btSphereShape(btScalar(1.));
-		collisionShapes.push_back(colShape);
+    //btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
+    btCollisionShape* colShape = new btSphereShape(btScalar(1.));
+    collisionShapes.push_back(colShape);
 
-		/// Create Dynamic Objects
-		btTransform startTransform;
-		startTransform.setIdentity();
+    /// Create Dynamic Objects
+    btTransform startTransform;
+    startTransform.setIdentity();
 
-		btScalar	mass(1.f);
+    btScalar  mass(1.f);
 
-		//rigidbody is dynamic if and only if mass is non zero, otherwise static
-		bool isDynamic = (mass != 0.f);
+    //rigidbody is dynamic if and only if mass is non zero, otherwise static
+    bool isDynamic = (mass != 0.f);
 
-		btVector3 localInertia(0,0,0);
-		if (isDynamic)
-			colShape->calculateLocalInertia(mass,localInertia);
+    btVector3 localInertia(0,0,0);
+    if (isDynamic)
+      colShape->calculateLocalInertia(mass,localInertia);
 
-			startTransform.setOrigin(btVector3(2,10,0));
-		
-			//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
-			btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
-			btRigidBody::btRigidBodyConstructionInfo rbInfo(mass,myMotionState,colShape,localInertia);
-			btRigidBody* body = new btRigidBody(rbInfo);
+      startTransform.setOrigin(btVector3(2,10,0));
+    
+      //using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+      btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+      btRigidBody::btRigidBodyConstructionInfo rbInfo(mass,myMotionState,colShape,localInertia);
+      btRigidBody* body = new btRigidBody(rbInfo);
 
-			dynamicsWorld->addRigidBody(body);
-	}
+      dynamicsWorld->addRigidBody(body);
+  }
 
 
 
 /// Do some simulation
 
 
-	///-----stepsimulation_start-----
-	for (i=0;i<100;i++)
-	{
-		dynamicsWorld->stepSimulation(1.f/60.f,10);
-		
-		//print positions of all objects
-		for (int j=dynamicsWorld->getNumCollisionObjects()-1; j>=0 ;j--)
-		{
-			btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[j];
-			btRigidBody* body = btRigidBody::upcast(obj);
-			if (body && body->getMotionState())
-			{
-				btTransform trans;
-				body->getMotionState()->getWorldTransform(trans);
-                                std::cout
-                                  << "world pos = "
-                                  << float(trans.getOrigin().getX()) << ","
-                                  << float(trans.getOrigin().getY()) << ","
-                                  << float(trans.getOrigin().getZ()) << "\n"
-                                  ;
-			}
-		}
-	}
+  ///-----stepsimulation_start-----
+  for (i=0;i<100;i++)
+  {
+    dynamicsWorld->stepSimulation(1.f/60.f,10);
+    
+    //print positions of all objects
+    for (int j=dynamicsWorld->getNumCollisionObjects()-1; j>=0 ;j--)
+    {
+      btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[j];
+      btRigidBody* body = btRigidBody::upcast(obj);
+      if (body && body->getMotionState())
+      {
+        btTransform trans;
+        body->getMotionState()->getWorldTransform(trans);
+        std::cout
+          << "world pos = "
+          << float(trans.getOrigin().getX()) << ","
+          << float(trans.getOrigin().getY()) << ","
+          << float(trans.getOrigin().getZ()) << "\n"
+          ;
+      }
+    }
+  }
 
-	///-----stepsimulation_end-----
+  ///-----stepsimulation_end-----
 
-	//cleanup in the reverse order of creation/initialization
-	
-	///-----cleanup_start-----
+  //cleanup in the reverse order of creation/initialization
+  
+  ///-----cleanup_start-----
 
-	//remove the rigidbodies from the dynamics world and delete them
-	for (i=dynamicsWorld->getNumCollisionObjects()-1; i>=0 ;i--)
-	{
-		btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[i];
-		btRigidBody* body = btRigidBody::upcast(obj);
-		if (body && body->getMotionState())
-		{
-			delete body->getMotionState();
-		}
-		dynamicsWorld->removeCollisionObject( obj );
-		delete obj;
-	}
+  //remove the rigidbodies from the dynamics world and delete them
+  for (i=dynamicsWorld->getNumCollisionObjects()-1; i>=0 ;i--)
+  {
+    btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[i];
+    btRigidBody* body = btRigidBody::upcast(obj);
+    if (body && body->getMotionState())
+    {
+      delete body->getMotionState();
+    }
+    dynamicsWorld->removeCollisionObject( obj );
+    delete obj;
+  }
 
-	//delete collision shapes
-	for (int j=0;j<collisionShapes.size();j++)
-	{
-		btCollisionShape* shape = collisionShapes[j];
-		collisionShapes[j] = 0;
-		delete shape;
-	}
+  //delete collision shapes
+  for (int j=0;j<collisionShapes.size();j++)
+  {
+    btCollisionShape* shape = collisionShapes[j];
+    collisionShapes[j] = 0;
+    delete shape;
+  }
 
-	//next line is optional: it will be cleared by the destructor when the array goes out of scope
-	collisionShapes.clear();
+  //next line is optional: it will be cleared by the destructor when the array goes out of scope
+  collisionShapes.clear();
 
-	///-----cleanup_end-----
+  ///-----cleanup_end-----
 }
 
